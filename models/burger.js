8c2221c8ec473a8c276306dbdc5b6da4a1dd5918
mecaputy <martina.e.caputy@gmail.com>
2018-06-27 23:08:10 -0400

    edits

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,22 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-//   all: function(cb) {
-//     orm.all("burger", function(res) {
-//       cb(res);
-//     });
-//   },
-//   // The variables cols and vals are arrays.
-//   create: function(cols, vals, cb) {
-//     orm.create("cats", cols, vals, function(res) {
-//       cb(res);
-//     });
-//   },
-//   update: function(objColVals, condition, cb) {
-//     orm.update("cats", objColVals, condition, function(res) {
-//       cb(res);
-//     });
-//   }, 
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }, 
 //   delete: function(condition, cb) {
 //     orm.delete("cats", condition, function(res) {
 //       cb(res);
@@ -24,9 +24,5 @@ var burger = {
 //   }
 };
 
-// Export the database functions for the controller (catsController.js).
+
 module.exports = burger;
-
-
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-// Export at the end of the burger.js file.
